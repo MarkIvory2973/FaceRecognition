9a5d3f98a4f31ab69d646f64ead123dc275e5c89
Mark Ivory <markivory2973@gmail.com>
2024-11-16 22:32:45 +0800

    fix: fixed dataset loading problem

--- src/ResNet18/Dataset.py
+++ src/ResNet18/Dataset.py
@@ -13,15 +13,15 @@ class CelebA(torch.utils.data.Dataset):
 
     def __getitem__(self, i):
         # 选人
-        anchor_person = self.persons[i]                     # 选取第 i 个人
+        anchor_person = self.persons[i+1]                                           # 选取编号为 i+1 的人
         negative_person = anchor_person
         while negative_person == anchor_person:
-            negative_person = random.choice(self.persons)   # 随机选另一个人（除第 i 个人）
+            negative_person = self.persons[random.randint(1, len(self.persons))]    # 随机选另一个人（除编号为 i+1 的人）
         
         # 选人的图片
-        anchor_path = self.root / random.choice(anchor_person)         # 在第 i 个人中随机选取一张人脸作为锚点
-        positive_path = self.root / random.choice(anchor_person)       # 在第 i 个人中随机选取一张人脸作为正例
-        negative_path = self.root / random.choice(negative_person)     # 在另一个人（非第 i 个人）中随机选取一张人脸作为反例
+        anchor_path = self.root / random.choice(anchor_person)         # 在编号为 i+1 的人中随机选取一张人脸作为锚点
+        positive_path = self.root / random.choice(anchor_person)       # 在编号为 i+1 的人随机选取一张人脸作为正例
+        negative_path = self.root / random.choice(negative_person)     # 在另一个人（非编号为 i+1 的人）中随机选取一张人脸作为反例
         
         # 打开图片
         anchor = Image.open(anchor_path).convert('RGB')
